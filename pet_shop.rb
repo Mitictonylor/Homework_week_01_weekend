80792bb46fb1986ba0f4577379d8a0400afa512f
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-13 17:40:34 +0000

    add add_pet_to_customer function

--- pet_shop.rb
+++ pet_shop.rb
@@ -72,3 +72,8 @@ end
 def customer_pet_count(customer)
   return customer[:pets].length
 end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+  return customer[:pets].length
+end
