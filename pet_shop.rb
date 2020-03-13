b0906497c0cf3e7dfa3624f6daa15cabd3043b4d
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-13 14:39:45 +0000

    add add_or_remove_cash function

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,11 @@ end
 def total_cash(who_got_the_money)
   return who_got_the_money[:admin][:total_cash]
 end
+
+def add_or_remove_cash(shop,cash_amount)
+shop[:admin][:total_cash] += cash_amount
+return shop[:admin][:total_cash]
+end
+
+def pets_sold()
+end
